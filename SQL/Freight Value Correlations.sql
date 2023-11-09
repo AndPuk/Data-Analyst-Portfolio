1b0cd34cfd341149b6177dd3b9563272046aeae6
AndPuk <124035807+AndPuk@users.noreply.github.com>
2023-11-09 12:55:57 +0200

    Update Freight Value Correlations.sql

--- SQL/Freight Value Correlations.sql
+++ SQL/Freight Value Correlations.sql
@@ -1,1 +1,14 @@
-
+SELECT	
+items.freight_value,	
+items.price,	
+products.product_weight_g,	
+products.product_length_cm,	
+products.product_height_cm,	
+products.product_width_cm	
+	
+FROM `olist_db.olist_order_items_dataset` AS items	
+JOIN `olist_db.olist_products_dataset` AS products	
+ON items.product_id = products.product_id	
+	
+WHERE products.product_weight_g IS NOT NULL	
+AND items.freight_value > 0	
